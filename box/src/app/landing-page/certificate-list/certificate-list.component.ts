8be58884a693eca61e5cb63a83bc1fe4eed357d3
box-lin <boxiang.lin@wsu.edu>
2024-07-03 12:29:28 -0500

    try routing with githubpage

---
+++ box/src/app/landing-page/certificate-list/certificate-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificate-list',
+  templateUrl: './certificate-list.component.html',
+  styleUrl: './certificate-list.component.css'
+})
+export class CertificateListComponent {
+
+}
---
+++ box/src/app/landing-page/landing-page.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CertificateListComponent } from '../certificate-list/certificate-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CertificateListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LandingPageModule { }
