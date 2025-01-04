e1e1ff2358185bcd77fe691386e777ddd024cabf
box-lin <theboxlin@gmail.com>
2025-01-03 22:08:01 -0800

    add leetcode CN fetch to scheduler

--- scheduler/leetcode-stat/app.js
+++ scheduler/leetcode-stat/app.js
@@ -1,10 +1,64 @@
 import { LeetCode } from "leetcode-query";
 import { promises as fs } from 'fs';
 
-async function fetchAndSaveRecentSubmissions(username) {
+async function recentSubmissionFromCN(username) {
+  const GRAPHQL_ENDPOINT = 'https://leetcode.cn/graphql/noj-go/';
+  const operationName = "recentAcSubmissions";
+  const query = `
+    query recentAcSubmissions($userSlug: String!) {
+      recentACSubmissions(userSlug: $userSlug) {
+        submissionId
+        submitTime
+        question {
+          title
+          translatedTitle
+          titleSlug
+          questionFrontendId
+        }
+      }
+    }
+  `;
+  const variables = { userSlug: username};
+  try {
+    const response = await fetch(GRAPHQL_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        operationName,
+        query,
+        variables
+      }),
+    });
+    let ret = []
+    const responseData = await response.json();
+    responseData.data.recentACSubmissions.forEach(element => {
+      ret.push({
+        title: element.question.title,
+        titleSlug: element.question.titleSlug,
+        timestamp: element.submitTime.toString(),
+        statusDisplay: "Accepted",
+        lang: "LeetcodeCN Graphql Not Supported"
+      })
+    });
+    return ret;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+async function fetchAndSaveRecentSubmissions(username, usernameCN) {
   const leetcode = new LeetCode();
   try {
-    const recentSubmissions = await leetcode.recent_submissions(username);
+    const [x,y] = await Promise.all([
+      leetcode.recent_submissions(username),
+      recentSubmissionFromCN(usernameCN)
+    ]);
+
+    const ret = [...x, ...y];
+    ret.sort((a,b) => Number(b.timestamp) - Number(a.timestamp));
+    const recentSubmissions = ret.slice(0, 10);
     console.log(recentSubmissions);
     
     // Write recentSubmissions to a JSON file
@@ -15,4 +69,8 @@ async function fetchAndSaveRecentSubmissions(username) {
   }
 }
 
-fetchAndSaveRecentSubmissions('boxlin');
+fetchAndSaveRecentSubmissions('boxlin', 'ddos-h');
+
+
+
+
