d99640b7bd746002cd72fadbe37194d94a09a264
box-lin <theboxlin@gmail.com>
2025-06-29 00:29:45 -0700

    vibe coding add css styles

--- box/src/app/ui/navigation-bar/navigation-bar.component.ts
+++ box/src/app/ui/navigation-bar/navigation-bar.component.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 import { NgClass } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [NgbNavModule, MatToolbarModule, MatButtonModule, NgClass, RouterModule, MatTab, MatTabGroup],
+  imports: [MatIconModule, NgClass, RouterModule],
   templateUrl: './navigation-bar.component.html',
-  styleUrl: './navigation-bar.component.css'
+  styleUrl: './navigation-bar.component.css',
 })
-export class NavigationBarComponent {
-
-}
+export class NavigationBarComponent {}
